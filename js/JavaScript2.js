7f8ff04414a7d9d98b82c5b5556f5d5fe23a000e
The_vio <105417303+MunzurCan@users.noreply.github.com>
2024-05-05 22:43:38 +0300

    Initial commit

---
+++ js/JavaScript2.js
@@ -0,0 +1,39 @@
+﻿// Müzik panelini açma ve kapama işlemlerini yöneten fonksiyon
+function tus() {
+    var panel = document.getElementById("MuzikPanel");
+    var button = document.getElementById("ac");
+    var icon = document.getElementById("icon");
+
+    if (window.getComputedStyle(panel).display === "none") {
+        acMuzikPanel(panel, button, icon);
+    } else {
+        kapatMuzikPanel(panel, button, icon);
+    }
+}
+
+// Müzik panelini açan fonksiyon
+function acMuzikPanel(panel, button, icon) {
+    panel.style.display = "block";
+    button.classList.remove("closed");
+    button.classList.add("open");
+}
+
+// Müzik panelini kapatan fonksiyon
+function kapatMuzikPanel(panel, button, icon) {
+    panel.style.display = "none";
+    button.classList.remove("open");
+    button.classList.add("closed");
+}
+
+// Butona tıklama olayını dinleyen fonksiyon
+function tusClick() {
+    var button = document.getElementById("ac");
+    button.addEventListener("click", function () {
+        tus();
+    });
+}
+
+// Sayfa yüklendiğinde gerekli olay dinleyicilerini etkinleştir
+document.addEventListener("DOMContentLoaded", function () {
+    tusClick();
+});
